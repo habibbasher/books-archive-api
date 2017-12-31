7e5b2fdbe9b5e6c9dab8633f45d86a7d70c67d44
habibbasher <habib0904010@gmail.com>
2017-12-31 13:42:37 +0600

    Reset password functionality is implemented

--- src/models/User.js
+++ src/models/User.js
@@ -34,6 +34,22 @@ schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
   return `${process.env.HOST}/confirmation/${this.confirmationToken}`;
 }
 
+schema.methods.generateResetPasswordUrl = function generateResetPasswordUrl() {
+  return `${process.env.HOST}/reset_password/${this.generateResetPasswordToken()}`;
+}
+
+schema.methods.generateResetPasswordToken = function generateResetPasswordToken() {
+  return jwt.sign(
+    {
+      _id: this._id
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h"
+    }
+  );
+}
+
 schema.methods.generateJWT = function generateJWT() {
   return jwt.sign(
     {
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,10 +3,12 @@
  */
 import express from 'express';
 const router = express.Router();
+import jwt from 'jsonwebtoken';
 /**
  * Importing custom modules
  */
 import User from '../models/User';
+import { sendResetPasswordEmail } from '../mailer';
 
 router.post("/", (req, res) => {
   const { credentials } = req.body;
@@ -28,5 +30,46 @@ router.post("/confirmation", (req, res) => {
     user ? res.json({ user: user.toAuthJSON() }): res.status(400).json({})
   );
 });
+
+router.post("/reset_password_request", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if(user) {
+      sendResetPasswordEmail(user);
+      res.json({});
+    } else {
+      res.status(400).json({ errors: { global: "There is no user with such email" } });
+    }
+  })
+});
+
+router.post("/validate_token", (req, res) => {
+  jwt.verify(req.body.token, process.env.JWT_SECRET, err => {
+    if (err) {
+      res.status(401).json({});
+    } else {
+      res.json({});
+    }
+  })
+});
+
+router.post("/reset_password", (req, res) => {
+  const { password, token } = req.body.data;
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      res.status(401).json({ errors: { global: "Invalid token" } });
+    } else {
+      User.findOne({ _id: decoded._id }).then(user => {
+        if (user) {
+          user.setPassword(password);
+          user.save().then(() => res.json({}));
+        } else {
+          res.status(404).json({ errors: { global: "Invalid token" } });
+        }
+      });
+    }
+  })
+});
+
+
 
 export default router;
