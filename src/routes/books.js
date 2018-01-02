d1cffda926b5adc8ee5d828190d0512d63e8f12e
habibbasher <habib0904010@gmail.com>
2018-01-02 14:33:43 +0600

    Some small modification

--- src/routes/books.js
+++ src/routes/books.js
@@ -12,18 +12,20 @@ router.get("/search", (req, res) => {
         title: "1984",
         authors: "Orwell",
         covers: [
-          "https://images.grpassets.com/books/13489905661/5470.jpg"
+          "https://images.gr-assets.com/books/13489905661/5470.jpg",
+          "https://images.gr-assets.com/books/15046119571/9577857.jpg"
         ],
         pages: 198
       },
       {
         goodreadsId: 2,
-        title: "The man",
-        authors: "The man in a Boat",
+        title: "Three Men in a Boat",
+        authors: "Jerome K. Jerome",
         covers: [
-          "https://images.grpassets.com/books/13489905661/5470.jpg"
+          "https://images.gr-assets.com/books/13927916561/4921.jpg",
+          "https://images.gr-assets.com/books/13120368781/627830.jpg"
         ],
-        pages: 200
+        pages: 256
       }
     ]
   });
